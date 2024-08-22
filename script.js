9b7336ea0155212848da43b9cdaeed7fca72082e
Douuglas1 <160265809+Douuglas1@users.noreply.github.com>
2024-08-22 15:35:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
-        const name = document.getElementById("name").value.trim();
-        const email = document.getElementById("email").value.trim();
-        const message = document.getElementById("message").value.trim();
+document.addEventListener("DOMContentLoaded", function() { 
+    // Seleciona o formulário na página
+    const form = document.querySelector("form"); 
 
-        if (name === "" || email === "" || message === "") {
-            alert("Por favor, preencha todos os campos.");
-            event.preventDefault(); // Evita o envio do formulário se houver campos vazios
-        }
-    });
-});
+    form.addEventListener("submit", function(event) { 
+        // Obtém os valores dos campos de entrada e remove espaços em branco
+        const name = document.getElementById("name").value.trim(); 
+        const email = document.getElementById("email").value.trim(); 
+        const message = document.getElementById("message").value.trim(); 
+
+        // Verifica se algum campo está vazio
+        if (name === "" || email === "" || message === "") { 
+            alert("Por favor, preencha todos os campos."); 
+            event.preventDefault(); // Evita o envio do formulário se houver campos vazios 
+        } 
+    }); 
+}); 
